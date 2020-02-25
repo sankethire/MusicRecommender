217a62d8f388a2937f8549f01a3523175b922c8d
Sanket Hire <sankethire98@gmail.com>
2020-02-25 16:50:36 +0530

    add the csv for frequency of tags

---
+++ sql/tags_frequency.py
@@ -0,0 +1,29 @@
+import csv
+import collections
+import operator
+
+filename = "artist_tags_1nf_clean.csv"
+
+fields = []
+tags = []
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+    for row in csvreader:
+        tags.append(row[1])
+
+tags_frequency = dict(collections.Counter(tags))
+tags_frequency = dict(sorted(tags_frequency.items(),key=operator.itemgetter(1),reverse=True))
+new_filename = "tags_freq.csv"
+
+# print(type(tags_frequency))
+
+tagfields = ['tags', 'frequency']
+with open(new_filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(tagfields)
+    for r in tags_frequency:
+        csvwriter.writerow([r,tags_frequency[r]])
+
+
+
