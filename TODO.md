0b55a4f5c6a16b00a6230621872e7ad4a28c8b4d
Yash Malviya <yashmalviya98@gmail.com>
2020-02-27 21:20:00 +0530

    table creation query - keys done

--- TODO.md
+++ TODO.md
@@ -2,5 +2,10 @@
    1. Keys
    2. Index
    3. Triggers
-2. Fake user data
-3. Queries
+      1. adding artist update tags
+2. Indexing
+   1. by artist name in table artists
+3. regenerate csv 
+   1. artist_clean
+4. Fake user data
+5. Queries
--- queries/tables.sql
+++ queries/tables.sql
@@ -1,19 +1,96 @@
-create table users(userid integer unique, username text unique, email text unique, passwd text, primary key (userid));
+create table artists(
+  mbid varchar(37),
+  artist_name text unique,
+  country text,
+  listeners integer,
+  scrobbles integer,
+  primary key (mbid)
+);
 
-create table user_details(userid integer unique, first_name varchar(32), last_name varchar(32), primary key (userid));
+create table songs(
+  track text,
+  artist text,
+  uri varchar(36),
+  danceability decimal,
+  energy decimal,
+  track_key integer,
+  loudness decimal,
+  mode decimal,
+  speechiness decimal,
+  acousticness decimal,
+  instrumentalness decimal,
+  liveness decimal,
+  valence decimal,
+  tempo decimal,
+  duration_ms integer,
+  time_signature integer,
+  chorus_hit decimal,
+  sections integer,
+  hit_target integer,
+  primary key (uri),
+  foreign key artist references artists(artist_name) on delete cascade on update cascade
+);
 
-create table playlists(playlist_id integer unique, userid integer, primary key (playlist_id, userid));
+create table tags(
+  tag_id integer,
+  tag_name text unique,
+  freq integer,
+  primary key(tag_id)
+);
 
-create table playlist_tracks(playlist_id integer unique, track_uri varchar(36), primary key (playlist_id));
+create table artist_tags(
+  mbid varchar(37),
+  tag_id integer,
+  primary key (mbid, tag_id), 
+  foreign key mbid references artists(mbid) on delete cascade on update cascade,
+  foreign key tag_id references tags(tag_id) on delete cascade on update cascade
+);
 
-create table songs(track text, artist text, uri varchar(36), danceability decimal, energy decimal, track_key integer, loudness decimal, mode decimal, speechiness decimal, acousticness decimal, instrumentalness decimal, liveness decimal, valence decimal, tempo decimal, duration_ms integer, time_signature integer, chorus_hit decimal, sections integer, hit_target integer);
+create table users(
+  userid integer,
+  username text unique,
+  email text unique,
+  passwd text,
+  primary key (userid)
+);
 
-create table artists(mbid varchar(37), artist_name text, country text, listeners integer, scrobbles integer);
+create table user_details(
+  userid integer,
+  first_name varchar(32),
+  last_name varchar(32),
+  primary key (userid),
+  foreign key (userid) references users(userid) on delete cascade on update cascade
+);
 
-create table user_interest_tags(userid integer, tag_id integer, clicks integer);
+create table playlists(
+  playlist_id integer,
+  userid integer,
+  primary key (playlist_id),
+  foreign key (userid) references users(userid) on delete on update cascade
+);
 
-create table tags(tag_id integer, tag_name text, freq integer);
+create table playlist_tracks(
+  playlist_id integer,
+  track_uri varchar(36),
+  primary key (playlist_id, track_uri),
+  foreign key (playlist_id) references playlists(playlist_id) on delete cascade on update cascade
+  foreign key (track_uri) references songs(uri) on delete cascade on update cascade
+);
 
-create table artist_tags(mbid varchar(37), tag_id integer);
+create table user_interest_tags(
+  userid integer,
+  tag_id integer,
+  clicks integer,
+  primary key (userid, tag_id),
+  foreign key userid references users(userid) on delete cascade on update cascade,
+  foreign key tag_id references tags(tag_id) on delete cascade on update cascade
+);
 
-create table user_recent_tracks(userid integer, track_uri varchar(36), time_stamp timestamp);
+create table user_recent_tracks(
+  userid integer,
+  track_uri varchar(36),
+  time_stamp timestamp,
+  primary key (userid, track_uri),
+  foreign key userid references users(userid) on delete cascade on update cascade,
+  foreign key track_uri references songs(uri) on delete cascade on update cascade
+);
