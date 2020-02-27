da16d89bf929fc36f6f6314a7cc1f0b0548e99a2
Sanket Hire <sankethire98@gmail.com>
2020-02-27 22:31:49 +0530

    add raw tables

--- queries/raw_tables.sql
+++ queries/raw_tables.sql
@@ -3,21 +3,21 @@ CREATE TABLE artists_raw(mbid VARCHAR,artist_mb VARCHAR,artist_lastfm VARCHAR,co
 
 SET CLIENT_ENCODING TO 'utf8';
 
- \COPY artists FROM '../csv/raw/artists/artists.csv' WITH CSV HEADER;
+ \COPY artists_raw FROM '../csv/raw/artists/artists.csv' WITH CSV HEADER;
 
 -- songs_raw table --
 CREATE TABLE songs_raw(track VARCHAR,artist VARCHAR,uri VARCHAR,danceability DECIMAL,energy DECIMAL,key INTEGER,loudness DECIMAL,mode DECIMAL,speechiness DECIMAL,acousticness DECIMAL,instrumentalness DECIMAL,liveness DECIMAL,valence DECIMAL,tempo DECIMAL,duration_ms INTEGER,time_signature INTEGER,chorus_hit DECIMAL,sections INTEGER,target INTEGER);
 
 SET CLIENT_ENCODING TO 'utf8';
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-00s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-00s.csv' WITH CSV HEADER;
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-60s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-60s.csv' WITH CSV HEADER;
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-70s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-70s.csv' WITH CSV HEADER;
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-80s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-80s.csv' WITH CSV HEADER;
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-90s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-90s.csv' WITH CSV HEADER;
 
-\COPY songs FROM '../csv/raw/songs/dataset-of-10s.csv' WITH CSV HEADER;
+\COPY songs_raw FROM '../csv/raw/songs/dataset-of-10s.csv' WITH CSV HEADER;
