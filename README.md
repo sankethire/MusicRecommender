e791b1884cdbe994f331823b95c5212f19eef639
Yash Malviya <yashmalviya98@gmail.com>
2020-02-28 23:30:00 +0530

    before submit commit

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Music Recommendation System using postgres
 
 - Spotipy API: https://spotipy.readthedocs.io/en/2.9.0/#
 - MusicBrainz API: https://python-musicbrainzngs.readthedocs.io/en/v0.7.1/
+
+## Commands
+
+1. Dump DB: `pg_dump -d project -h localhost -U postgres > project.sql`
+2. Load DB: `psql -U postgres -d project -f project.sql` (Create DB named porject before hand)
--- queries/trigger.sql
+++ queries/trigger.sql
@@ -2,18 +2,22 @@ create or replace function trigger_add_recent_track()
 	returns trigger as 
 	$body$
 		begin
-			if (select count(*) from user_recent_tracks where user_recent_tracks.username = new.username) > 10 then
+			if (select count(*) from user_recent_tracks where user_recent_tracks.username = new.username) >= 10 then
 				begin
 					delete from user_recent_tracks where new.username = user_recent_tracks.username and user_recent_tracks.time_stamp = (select min(time_stamp) from user_recent_tracks where new.username = user_recent_tracks.username);
 				end;
 			end if;
+
+			update user_interest_tags set clicks = clicks+1 where user_interest_tags.tag_name in ((select user_interest_tags.tag_name where user_interest_tags.username = new.username) intersect (select artist_tags.tag_name from artist_tags where artist_tags.artist_name = (select artist from songs where songs.uri = new.track_uri)));
+
+			insert into user_interest_tags select new.username, q1.tag_name, 1 from ((select artist_tags.tag_name from artist_tags where artist_tags.artist_name = (select artist from songs where songs.uri = new.track_uri)) except (select user_interest_tags.tag_name from user_interest_tags where user_interest_tags.username = new.username)) as q1;
+
 			return new;
 		end
 	$body$
 language plpgsql;
 
 create trigger add_recent_track
-	before insert
+	before insert 
 	on user_recent_tracks
-	for each row
-	execute procedure trigger_add_recent_track();
+	for each row execute procedure trigger_add_recent_track();
